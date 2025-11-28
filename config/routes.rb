9722819282ec82380f46e1b7a9d21bef9cd8b4a7
SOFIA LARRAIN <sofialarrainv@uc.cl>
2025-11-28 13:02:13 -0300

    Add API routes for inventory management
    - Introduced routes for warehouses, products, and stocks within the API namespace.
    - Added endpoints for inventory movements, including entry, exit, and transfer operations.
    - Implemented custom routes for retrieving stock information by warehouse and product.

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,18 @@ Rails.application.routes.draw do
             }
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    resources :warehouses
+    resources :products
+    resources :stocks, only: [ :index, :show ]
+    get "stocks/by_warehouse/:warehouse_id", to: "stocks#by_warehouse"
+    get "stocks/by_product/:product_id", to: "stocks#by_product"
+    resources :inventory_movements, only: [ :index, :show ]
+    post "inventory/entry", to: "inventory_movements#register_entry"
+    post "inventory/exit", to: "inventory_movements#register_exit"
+    post "inventory/transfer", to: "inventory_movements#register_transfer"
+    get "inventory/history/:product_id", to: "inventory_movements#movement_history"
+  end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
